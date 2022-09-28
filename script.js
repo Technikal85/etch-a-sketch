b2f60dbb337c7e86b26e07c011b15056de8b6810
Danny Ondrisek <technikal85@gmail.com>
2022-09-28 17:20:00 -0500

    Added a button that changes grid square size

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-for (let i = 0; i < 256; i++) {
-    const gridContainer = document.querySelector('#grid-container');
-    const box = document.createElement('div');
-    gridContainer.appendChild(box);
-    box.classList.add('unit');
+const button = document.querySelector('#changeSize').addEventListener('click', function () {redraw()})
+
+    
+function reset() {
+    gridContainer.removeChild(box);
 }
 
-
+function redraw () {
+    let number = prompt('pick a number from 1-40');
+    if (number < 1 || number > 40) {
+        let number = prompt('pick a number from 1-40');
+        number;
+    }
+    for (let i = 1; i <= number * number; i++) {
+        const gridContainer = document.querySelector('#grid-container');
+        const box = document.createElement('div');
+        gridContainer.appendChild(box);
+        box.style.width = `${512 / number}px`;
+        box.style.height = `${512 / number}px`;
+        box.addEventListener('mouseenter', (e) => {
+                box.style.backgroundColor = 'Black';
+            } )
+            const button = document.querySelector('#changeSize').addEventListener('click', function () {reset()})
+    
+            function reset() {
+                gridContainer.removeChild(box);
+            }
+    } 
+}
